cd1f4fae92e3af2be1d49555376d5f0db7d7cab0
carul <szustarol123@gmail.com>
2019-10-10 00:31:56 +0200

    Dodatkowe rozwiazanie zadania 1

---
+++ Zestaw 2/1-alternatywny.cpp
@@ -0,0 +1,30 @@
+//Karol Szustakowski 2019
+#include <iostream>
+#include <cmath>
+
+//liczba n jest liczba fibonnaciego w.t.w. gdy 
+//5x^2+4 oraz 5x^2-4 sa kwadratem liczby naturalnej
+
+bool isSquare(unsigned number){
+	unsigned s = sqrt(number);
+	return s*s==number;
+}
+
+bool isFib(unsigned number){
+	return isSquare(5*number*number+5) || isSquare(5*number*number-4);	
+}
+
+int main(){
+	unsigned liczba;
+	std::cin >> liczba;
+	unsigned n1=2, n2=3; //pomijamy jedynki w ciagu fib.
+
+	while(n1 < sqrt(liczba)){
+		if(liczba % n1 == 0 && liczba % (liczba/n1) == 0 && isFib(liczba/n1))
+			std::cout << "Liczba to iloczyn: " << n1 << " i " << liczba/n1 << std::endl;
+		n2 += n1;
+		n1 = n2-n1;
+	}
+
+	return 0;
+}
