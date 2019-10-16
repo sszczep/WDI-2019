c2ec82933f241fd69248a834c7a730c5cc15fac2
Jakub Gonet <jakub.gonet@swmansion.com>
2019-10-16 08:54:38 +0200

    Improv 10
    - remove unnecessary inner loop

--- Zestaw 2/10.cpp
+++ Zestaw 2/10.cpp
@@ -1,30 +1,19 @@
 #include <iostream>
-#include <cmath>
 
-double f(double x){
-	return 1/x;
-}
+double f(double x) { return 1 / x; }
 
-int main(){
-	double k;
-	double dx;
-	std::cout << "Podaj k: ";
-	std::cin >> k;
+int main() {
+  double k;
+  std::cout << "Podaj k: ";
+  std::cin >> k;
 
-	dx = k/2;
+  double dx = 1e-7;
 
-	double area = 0;
-	while(dx > 1e-7){
-		for(double i = 1; i < k; i+= dx){
-			area += dx * f(i+dx);
-		}
-		dx /= 2;
+  double area = 0;
+  for (double i = 1; i < k; i += dx) {
+    area += dx * f(i + dx);
+  }
+  std::cout << "dX: " << dx << " => " << area << std::endl;
 
-		std::cout << "dX: " << dx << " => " << area << '\r' << std::flush;
-		area = 0;
-	}
-
-	std::cout << std::endl;
-
-	return 0;
+  return 0;
 }
