83bcfd28321b27e105629acd9a7cb148355be8a5
Jakub Gonet <jakub.gonet@swmansion.com>
2019-10-22 22:30:20 +0200

    Improv 3/03

--- Zestaw 3/03.cpp
+++ Zestaw 3/03.cpp
@@ -1,36 +1,38 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-int n;
-
+// Napisać program generujący i wypisujący liczby pierwsze mniejsze
+// od N metodą Sita Eratostenesa
 int main() {
+    int n;
     cin >> n;
     cout << endl << endl;
 
-    if (n < 3) {
-        return 0;
+    bool *is_prime = new bool[n];
+
+    // domyślna wartosc - jest pierwszą
+    for(size_t i = 0; i < n; ++i){
+        is_prime[i] = true;
     }
 
-    bool* liczby = new bool[n];
-
-    for (int i = 2; i < n; i++) {
-        liczby[i] = true;
-    }
-    
-    for (int i = 2; i * i <= n; i++) {
-        if (!liczby[i]) {
-            continue;
+    // wykreslamy kolejne wielokrotnosci liczb pierwszych zaczynajac od 2
+    // sprawdzamy tylko do sqrt(n) jak Gajecki przykazal przy liczbach pierwszych
+    for(size_t i = 2; i < sqrt(n); ++i){
+        // jezeli nie zostala wykreslona wczesniej
+        if(is_prime[i]){
+            // to wykreslamy jej kolejne wielokrotnosci
+            for(size_t k = 2*i; k < n; k += i){
+                is_prime[k] = false;
+            }
         }
-
-        for (int j = i * 2; j < n; j += i) {
-            liczby[j] = false;
-        }
     }
 
-    for (int i = 2; i < n; i++) {
-        if (liczby[i]) {
-            cout << i << endl;
+    // wypisujemy liczby pierwsze zaczynajac od 2
+    for(size_t i = 2; i < n; ++i){
+        if(is_prime[i]) {
+            cout << i << endl;;
         }
     }
 }
