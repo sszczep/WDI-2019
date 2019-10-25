305b7be4519901e69124b0b357a9f380cb0a4bc8
Sebastian Szczepanski <s.szczepanski00@gmail.com>
2019-10-25 14:47:16 +0200

    Dodano 03/10 i 03/11

---
+++ Zestaw 3/11.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+const int MAX = 1000;
+const int START_NATURALNYCH = 1;
+const int KONIEC_NATURALNYCH = 1000;
+
+int main() {
+    // Konfigurujemy generator pseudolosowy
+    srand(time(NULL));
+
+    int t[MAX];
+    for(int i = 0; i < MAX; i++) {
+        // Wypełniamy tablicę pseudolosowymi liczbami z przedziału 1-1000
+        t[i] = (rand() % (KONIEC_NATURALNYCH - START_NATURALNYCH + 1)) + START_NATURALNYCH;
+    }
+
+    int najdluzszyPodciag = 2;
+    int obecnaDlugosc = 2;
+    for(int i = 2; i < MAX; i++) {
+        if(t[i] - t[i - 1] == t[i - 1] - t[i -2]) obecnaDlugosc++;
+        else {
+            if(obecnaDlugosc > najdluzszyPodciag) najdluzszyPodciag = obecnaDlugosc;
+            obecnaDlugosc = 2;
+        }
+    }
+
+    if(obecnaDlugosc > najdluzszyPodciag) najdluzszyPodciag = obecnaDlugosc;
+
+    std::cout << najdluzszyPodciag;
+}
