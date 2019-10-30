ed5a7ee2d199539f107514f97a40044a292b0283
anuar2k <anuar2k@outlook.com>
2019-10-30 22:27:58 +0100

    Poprawiono bug, formatowanie i output w zad. 1 / zestaw 3

--- Zestaw 3/01.cpp
+++ Zestaw 3/01.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
+#include <iomanip>
 
 // Napisać program zamieniający liczbę naturalną z systemu 10 na
 // podstawę 2-16
 using namespace std;
 
 void print_in_base(unsigned n, unsigned base) {
-  const string glyphs = "0123456789abcef";
-  string transformed = "";
-  while (n > 0) {
-    transformed += glyphs[n % base];
-    n /= base;
-  }
+    const string glyphs = "0123456789abcdef";
+    string transformed = "";
+    while (n > 0) {
+        transformed += glyphs[n % base];
+        n /= base;
+    }
 
-  for (int i = transformed.length() - 1; i >= 0; --i) {
-    cout << transformed[i];
-  }
-  cout << endl;
+    cout << setw(2) << base << " - ";
+    for (int i = transformed.length() - 1; i >= 0; --i) {
+        cout << transformed[i];
+    }
+    cout << endl;
 }
 
 int main() {
-  unsigned n;
-  cin >> n;
+    unsigned n;
+    cin >> n;
+    cout << setfill('0');
 
-  for (size_t i = 2; i <= 16; ++i) {
-    print_in_base(n, i);
-  }
+    for (size_t i = 2; i <= 16; ++i) {
+        print_in_base(n, i);
+    }
 }
