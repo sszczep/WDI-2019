59d0056d6e73175be1a571d45384914d078ee93c
Sebastian Szczepański <s.szczepanski00@gmail.com>
2019-11-26 23:39:25 +0100

    Update 07.cpp

--- Zestaw06-08/07.cpp
+++ Zestaw06-08/07.cpp
@@ -1,43 +1,28 @@
 #include <iostream>
-using namespace std;
 
 const int N = 6;
 
-//mMin = -1;
-//podzbior(t, 0, "", mMin);
-void podzbior(int t[N], int n, string kombinacja, int& mMin, int& s) {
-    if (n == N) {
-        int sumaElem = 0;
-        int sumaIdx = 0;
-        int moc = 0;
-        for (int i = 0; i < N; i++) {
-            if (kombinacja[i] == '1') {
-                sumaElem += t[i];
-                sumaIdx += i;
-                moc++;
-            }
-        }
-        if (moc > 0 && sumaElem == sumaIdx) {
-            if (mMin == -1 || moc < mMin) {
-                mMin = moc;
-                s = sumaElem;
-            }
-        }
-    }
-    else {
-        podzbior(t, n + 1, kombinacja + '0', mMin, s);
-        podzbior(t, n + 1, kombinacja + '1', mMin, s);
-    }
-}
+bool najmniejszaSuma(int t[], int &sum, int valueSum = 0, int indexSum = 0, int currentIndex = 0) {
+  // Jeżeli sumy są sobie równe i są różne od zera to zwracamy prawdę
+  if(valueSum == indexSum && valueSum != 0) {
+    sum = valueSum;
+    return true;
+  }
 
-int najmniejszy(int t[N]) {
-    int mMin = -1;
-    int s = -1;
-    podzbior(t, 0, "", mMin, s);
-    return s;
+  // Jeżeli osiągneliśmy koniec tablicy to zwracamy fałsz
+  if(currentIndex == N) return false;
+
+  return
+    najmniejszaSuma(t, sum, valueSum, indexSum, currentIndex + 1)
+    || najmniejszaSuma(t, sum, valueSum + t[currentIndex], indexSum + currentIndex, currentIndex + 1);
 }
 
 int main() {
-    int t[N] = {1, 7, 3, 5, 11, 2};
-    cout << najmniejszy(t);
+  int t[N] = {1, 7, 3, 5, 11, 2};
+
+  int sum = 0;
+  bool exists = najmniejszaSuma(t, sum);
+
+  if(exists) std::cout << sum;
+  else std::cout << "Brak takiej sumy";
 }
