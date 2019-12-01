9b3167301c06ce61dcf6effa33bd8e9635664657
anuar2k <anuar2k@outlook.com>
2019-12-01 13:56:16 +0100

    Dodano zadanie 2 z kartkowki 1 rok 17/18 grupa a

---
+++ Kartkówki/2017-2018/K1_A_ZAD2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+const int N = 5;
+int tab[N];
+
+int idx(int s, int k) {
+    int dl = k - s + 1;
+
+    for (int i = dl; i > 0; i--) {
+        for (int pocz = s; pocz <= k - dl + 1; pocz++) {
+            int sTab = 0;
+            int sIdx = 0;
+            for (int j = pocz; j < pocz + i; j++) {
+                sTab += tab[j];
+                sIdx += j;
+            }
+            if (sTab == sIdx) {
+                return i;
+            }
+        }
+    }
+    return 0;
+}
+
+int dl() {
+    int pocz = 0;
+    int maxDl = 0;
+    while (pocz < N) {
+        int s = pocz;
+        int k = pocz;
+        while (k < N - 1 && tab[pocz] < tab[pocz + 1]) {
+            pocz++;
+            k = pocz;
+        }
+        int dl = idx(s, k);
+        if (dl > maxDl) {
+            maxDl = dl;
+        }
+        pocz++;
+    }
+    return maxDl;
+}
+
+int main() {
+    for (int i = 0; i < N; i++) {
+        cin >> tab[i];
+    }
+    cout << dl();
+}
